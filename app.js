f7926437d1585d69aea27286568982595c8ab58b
Santos Gonzalez <sgonzo3@gmail.com>
2020-01-06 23:26:29 -0500

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// Doubly Linked Lists
+
+class Node {
+  constructor(data){
+    this.val = data;
+    this.prev = null;
+    this.next = null;
+  }
+}
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+// show  length
+  size() {
+
+  }
+
+// show head
+  peekHead() {
+
+  }
+
+// show tail
+  peekTail() {
+
+  }
+
+// add to head
+  unShift() {
+
+  }
+
+// add to tail
+  push() {
+
+  }
+// remove head
+  shift() {
+
+  }
+// remove tail
+  pop() {
+
+  }
+
+  get(index) {
+
+  }
+
+  set(index, data) {
+
+  }
+
+// remove at index
+  removeNodeAt(index) {
+
+  }
+
+// show all node values with pointer to next
+  print() {
+
+  }
+
+  reverseList() {
+
+  }
+
+  findCycle() {
+
+  }
+
+  removeDuplicates() {
+
+  }
+
+  findFromBack() {
+    // might just set negative value on get
+  }
+}
+
+// Tests
+const list = new DoublyLinkedList();
